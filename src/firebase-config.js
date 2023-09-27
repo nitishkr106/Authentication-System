b680d98eb3d8a71701db46e605665c5da5564c90
Nitish Kumar <60603263+nitishkr106@users.noreply.github.com>
2023-09-27 09:43:30 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBB9B7j78cSguHvTqxS8i4f8zlqNbSI1JM",
-    authDomain: "otp-app-demo-d728f.firebaseapp.com",
-    projectId: "otp-app-demo-d728f",
-    storageBucket: "otp-app-demo-d728f.appspot.com",
-    messagingSenderId: "244168306573",
-    appId: "1:244168306573:web:9c74623b3024d93d96d7c4"
+    apiKey: "guHvTqxS8i4f8zlqNbSI1",
+    authDomain: "emo-d728f.firebaseapp.com",
+    projectId: "-demo-d72",
+    storageBucket: "8f.appspot.com",
+    messagingSenderId: "3065",
+    appId: "06573:web:9024d93dd74"
 };
 
 const app = initializeApp(firebaseConfig);
